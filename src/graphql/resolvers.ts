55623b00b17d5eeef73bc7a69bdbadf782927f33
JaanJahimees <jaanjahilo@outlook.com>
2020-06-09 12:40:14 +0300

    Add dummy signup endpoint

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,6 +2,15 @@ const resolvers = {
     Query: {
         hello: () => 'Hello world!',
     },
+    Mutation: {
+        // TODO: Use proper types and add functionality
+        signup: (input: any) => {
+            return {
+                status: true,
+                token: null,
+            }
+        }
+    }
 };
 
 export default resolvers;
