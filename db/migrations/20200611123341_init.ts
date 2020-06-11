d7ccd6905056b3bd7477cbba3090ba20b566e057
JaanJahimees <jaanjahilo@outlook.com>
2020-06-11 13:01:00 +0300

    Add initial database migration

--- db/migrations/20200611123341_init.ts
+++ db/migrations/20200611123341_init.ts
@@ -2,9 +2,26 @@ import * as Knex from "knex";
 
 
 export async function up(knex: Knex): Promise<any> {
+    return knex.schema.createTable('users', table => {
+        // Primary key
+        table.increments().unsigned().primary();
+
+        // User name
+        table.string('name').unique().notNullable();
+
+        // User password
+        table.string('password').notNullable();
+
+        // User email
+        table.string('email').unique().notNullable();
+
+        // Timestamps 'updated_at' and 'created_at'
+        table.timestamps(true, true);
+    })
 }
 
 
 export async function down(knex: Knex): Promise<any> {
+    return knex.schema.dropTable('users');
 }
 
