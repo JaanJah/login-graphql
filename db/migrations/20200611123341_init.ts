e08a5d5df1103bbdd8a5aef569a807283c00893f
JaanJahimees <jaanjahilo@outlook.com>
2020-06-12 11:03:33 +0300

    Add sessions table to migrations

--- db/migrations/20200611123341_init.ts
+++ db/migrations/20200611123341_init.ts
@@ -4,7 +4,7 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<any> {
     return knex.schema.createTable('users', table => {
         // Primary key
-        table.increments().unsigned().primary();
+        table.increments('id').unsigned().primary();
 
         // User name
         table.string('name').unique().notNullable();
@@ -17,11 +17,31 @@ export async function up(knex: Knex): Promise<any> {
 
         // Timestamps 'updated_at' and 'created_at'
         table.timestamps(true, true);
-    })
+    }).then(async () => {
+        await knex.schema.createTable('sessions', table => {
+            // Primary key
+            table.increments('id').unsigned().primary();
+
+            // Session owner
+            table.integer('user_id')
+                .unsigned()
+                .references('id')
+                .inTable('users')
+                .notNullable()
+                .onDelete('cascade');
+
+            // Session token signature
+            table.string('signature').unique().notNullable();
+
+            // Timestamps 'updated_at' and 'created_at'
+            table.timestamps(true, true);
+        })
+    });
 }
 
 
 export async function down(knex: Knex): Promise<any> {
-    return knex.schema.dropTable('users');
+    return knex.schema.dropTable('sessions')
+        .then(async () => knex.schema.dropTable('users'));
 }
 
